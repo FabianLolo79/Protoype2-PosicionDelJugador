1431f078adbfc8f2ff6be4e8a0e54250d50f2332
Fabián E. García <fabianegl79@gmail.com>
2024-02-20 17:48:48 -0300

    metodo para generar animales a intervalos, colliders y triggers para destruir comida y animales

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,26 +6,30 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 16;
     private float spawnPositionZ = 20;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 2.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
+       
+    }
 
-            Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
 
-            Instantiate(animalPrefabs[animalIndex], spawnPosition, 
-                        animalPrefabs[animalIndex].transform.rotation);
-        }
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPosition,
+                    animalPrefabs[animalIndex].transform.rotation);
     }
 }
